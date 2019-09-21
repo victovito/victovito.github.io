d121d04cae1bc4bbd0f2fbb62ba625ac00f7d530
Victor Rosa <victoreosr@gmail.com>
2019-09-21 04:24:14 -0300

    Add all types of collision. Still thinking it is too confusing and messy, so I realy want to remake it

--- classes/player.js
+++ classes/player.js
@@ -30,12 +30,12 @@ class Player
     UpdatePlayer(){
         this.Draw();
         
-        let movement = new Vector2();
+        this.velocity.x = 0;
         if (Input.right){
-            movement.x += 1;
+            this.velocity.x += this.speed * deltaTime;
         }
         if (Input.left){
-            movement.x -= 1;
+            this.velocity.x -= this.speed * deltaTime;
         }
 
         if (this.inGround){
@@ -43,51 +43,120 @@ class Player
                 this.velocity.y = 0.15;
             }
         }
-
-        this.position = this.position.Add(movement.Scale(this.speed * deltaTime));
-        this.UpdateVelocity();
+        this.position = this.position.Add(new Vector2(this.GetMoveAmountX(), this.GetMoveAmountY()));
+        this.GravityForce();
 
     }
 
-    GetFallAmount(){
-
-        let contactPoints = [this.position.x - this.size.x/2, this.position.x, this.position.x + this.size.x / 2];
+    GetMoveAmountX(){
+        let contactPoints = [
+            this.position.y,
+            this.position.y + this.size.y / 2,
+            this.position.y + this.size.y
+        ]
         let dists = [];
-        
-        for (let cp of contactPoints){
-            for (let y = this.position.y; true; --y){
-                if (y < 0){
-                    break;
-                }
-                let block = world.GetBlockAt(new Vector2Int(cp, y));
-                if (block == undefined){
-                    continue;
-                }
-                if (BLOCK.properties[block.type].isSolid){
-                    dists.push(-(this.position.y - block.position.y - 1));
-                    break;
-                } else {
-                    continue;
+        if (this.velocity.x > 0){
+            for (let cp of contactPoints){
+                for (let x = this.position.x + this.size.x/2; true; x++){
+                    let block = world.GetBlockAt(new Vector2Int(x, cp));
+                    if (block == undefined){
+                        break;
+                    }
+                    if (BLOCK.properties[block.type].isSolid){
+                        dists.push(block.position.x - (this.position.x + this.size.x/2) - 0.01);
+                        break;
+                    }
                 }
             }
+            let moveAmount = Math.min.apply(Math, dists);
+            return Math.min(moveAmount, this.velocity.x);
         }
-        let fallAmount = Math.max.apply(Math, dists);
-        if (fallAmount >= 0){
-            if (!this.inGround){
-                this.velocity = new Vector2();
+        else if (this.velocity.x < 0){
+            for (let cp of contactPoints){
+                for (let x = this.position.x - this.size.x/2; true; x--){
+                    let block = world.GetBlockAt(new Vector2Int(x, cp));
+                    if (block == undefined){
+                        break;
+                    }
+                    if (BLOCK.properties[block.type].isSolid){
+                        dists.push(block.position.x + 1 - (this.position.x - this.size.x/2) + 0.01);
+                        break;
+                    }
+                }
             }
-            this.inGround = true;
+            let moveAmount = Math.max.apply(Math, dists);
+            return Math.max(moveAmount, this.velocity.x);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    GetMoveAmountY(){
+        if (this.velocity.y <= 0){
+            let contactPoints = [this.position.x - this.size.x / 2,
+                this.position.x, this.position.x + this.size.x / 2];
+            let dists = [];
+            for (let cp of contactPoints){
+                for (let y = this.position.y; true; y--){
+                    if (y < 0){
+                        break;
+                    }
+                    let block = world.GetBlockAt(new Vector2Int(cp, y));
+                    if (block == undefined){
+                        continue;
+                    }
+                    if (BLOCK.properties[block.type].isSolid){
+                        dists.push(-(this.position.y - block.position.y - 1));
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            let moveAmount = Math.max.apply(Math, dists);
+            if (moveAmount >= 0){
+                if (!this.inGround){
+                    this.velocity.y = 0;
+                }
+                this.inGround = true;
+            } else {
+                this.inGround = false;
+            }
+            return Math.max(moveAmount, this.velocity.y);
         } else {
             this.inGround = false;
+            let contactPoints = [this.position.x - this.size.x/2, this.position.x, this.position.x + this.size.x / 2];
+            let dists = [];
+            for (let cp of contactPoints){
+                for (let y = this.position.y + this.size.y; true; y++){
+                    if (y > world.chunkSizeY){
+                        break;
+                    }
+                    let block = world.GetBlockAt(new Vector2Int(cp, y));
+                    if (block == undefined){
+                        continue;
+                    }
+                    if (BLOCK.properties[block.type].isSolid){
+                        dists.push(block.position.y - (this.position.y + this.size.y) - 0.01);
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            let moveAmount = Math.min.apply(Math, dists);
+            if (moveAmount <= 0.01){
+                this.velocity.y = 0;
+            }
+            return Math.min(moveAmount, this.velocity.y);
         }
-        return Math.max(fallAmount, this.velocity.y);
     }
 
-    UpdateVelocity(){
+    GravityForce(){
         if (!this.inGround){
             this.velocity = this.velocity.Sub(new Vector2(0, world.gravity / 1000));
         }
-        this.position = this.position.Add(new Vector2(0, this.GetFallAmount()));
     }
 
     Draw(){
